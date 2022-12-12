98ea68e588b8298a80ef60901e20386cf6807606
Craig <cdevelopment010@gmail.com>
2022-12-12 19:26:46 +0000

    Feat: score and overlap
    Added functionality to check the boxes overlap with the square at
    the bottom. It will remove the box and update the score if they do
    overlap.

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ setInterval( () => {
     generateBox('box-1', Math.random()*document.querySelector('.container').clientWidth, '3s' )
 }, 2000)
 
+setInterval( ()=> {
+    checkCollision();
+}, 100) 
+
 window.addEventListener('keydown', function(e) {
     if(e.code === 'ArrowRight') {
         moveRudolph('right'); 
@@ -17,7 +21,6 @@ window.addEventListener('keydown', function(e) {
 
 
 function moveRudolph(direction) {
-    console.log(rudolph);
     let rLeft = window.getComputedStyle(rudolph)['left'].split('px')[0]*1;
     let rWidth = window.getComputedStyle(rudolph)['width'].split('px')[0]*1;
     let clientWidth = document.querySelector('.container').clientWidth; 
@@ -53,4 +56,37 @@ function generateBox(box, position, speed) {
     setTimeout(() => {
         span.remove();
     }, speed.split('s')[0] * 1000)
+}
+
+
+
+function checkCollision() {
+    let presents = document.querySelectorAll('.box'); 
+    let rudoplhRect = rudolph.getBoundingClientRect(); 
+    presents.forEach( p => {
+        let pRect = p.getBoundingClientRect(); 
+        // console.log (pRect.bottom <= rudoplhRect.top) //works
+        if (pRect.bottom <= rudoplhRect.top ) { return}; 
+        if (pRect.bottom <= rudoplhRect.bottom && 
+            pRect.bottom >= rudoplhRect.top && 
+            pRect.left <= rudoplhRect.right &&
+            pRect.right >= rudoplhRect.left
+            ) {
+                // add score
+                    console.log("Add to score!");
+                    addToScore();
+                    // console.log(p);
+                    // return;
+                // remove present
+                p.remove();
+            }
+    })
+}
+
+
+function addToScore() {
+    let score = document.getElementById('score').innerText.split(":")[1]*1; 
+    score++;
+    document.getElementById('score').innerText = `Score: ${score}`; 
+
 }
